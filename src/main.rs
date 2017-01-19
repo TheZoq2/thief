e615db6adf7ed932164668547971b4d716e378b3
TheZoq2 <frans.skarman@gmail.com>
2017-01-19 23:37:30 +0100

    Add glium to draw screenshot

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,23 @@
 extern crate x11;
 extern crate libc;
 
+#[macro_use]
+extern crate glium;
+
+use std::io::Cursor;
+
 use x11::xlib;
 use std::mem;
+
+use glium::texture::RawImage2d;
+
+type Pixel = (u8, u8, u8);
 
 pub struct Screenshot
 {
     width: u32,
     height: u32,
-    pixels: Vec<(u8, u8, u8)>
+    pixels: Vec<Pixel>
 }
 
 impl Screenshot
@@ -29,9 +38,15 @@ impl Screenshot
         }
     }
 
-    fn push_pixel(&mut self, pixel: (u8, u8, u8))
+    fn push_pixel(&mut self, pixel: Pixel)
     {
         self.pixels.push(pixel)
+    }
+
+    fn get_glium_image<'a>(self) -> RawImage2d<'a, Pixel>
+    {
+        let dimensions = (self.width, self.height);
+        RawImage2d::from_raw_rgb(self.pixels, dimensions)
     }
 }
 
@@ -92,9 +107,94 @@ pub fn capture_screenshot() -> Screenshot
 }
 
 fn main() {
-    println!("Hello, world!");
+    use glium::{DisplayBuild, Surface};
+    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
 
     let screenshot = capture_screenshot();
+
+    //let image = glium::texture::RawImage2d::from_raw_rgba_reversed(image.into_raw(), dimensions);
+    let image = screenshot.get_glium_image();
+    let texture = glium::texture::Texture2d::new(&display, image).unwrap();
+
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+        tex_coords: [f32; 2],
+    }
+
+    implement_vertex!(Vertex, position, tex_coords);
+
+    let shape = vec![
+            //First triangle
+            Vertex { position: [0., 0.], tex_coords: [0., 0.] },
+            Vertex { position: [0., 1.], tex_coords: [0., 1.] },
+            Vertex { position: [1., 0.], tex_coords: [1., 0.] },
+            //Second triangle                                
+            Vertex { position: [0., 1.], tex_coords: [0., 1.] },
+            Vertex { position: [1., 1.], tex_coords: [1., 1.] },
+            Vertex { position: [1., 0.], tex_coords: [1., 0.] },
+        ];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 140
+        in vec2 position;
+        in vec2 tex_coords;
+        out vec2 v_tex_coords;
+        uniform mat4 matrix;
+        void main() {
+            v_tex_coords = tex_coords;
+            gl_Position = matrix * vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+        in vec2 v_tex_coords;
+        out vec4 color;
+        uniform sampler2D tex;
+        void main() {
+            color = texture(tex, v_tex_coords);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+    let mut t = -0.5;
+
+    loop {
+        // we update `t`
+        t += 0.0002;
+        if t > 0.5 {
+            t = -0.5;
+        }
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+        let uniforms = uniform! {
+            matrix: [
+                [1.0 , 0.0, 0.0, 0.0],
+                [0.0 , 1.0, 0.0, 0.0],
+                [0.0 , 0.0, 1.0, 0.0],
+                [-0.5, -0.5, 0.0, 1.0f32],
+            ],
+            tex: &texture,
+        };
+
+        target.draw(&vertex_buffer, &indices, &program, &uniforms,
+                    &Default::default()).unwrap();
+        target.finish().unwrap();
+
+        for ev in display.poll_events() {
+            match ev {
+                glium::glutin::Event::Closed => return,
+                _ => ()
+            }
+        }
+    }
 }
 
 
